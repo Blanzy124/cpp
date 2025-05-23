9d24f38719476cdfd28758ab382760cb63d7bfb0
Blanzy124 <samuelmedinablandon124@gmail.com>
2025-05-22 21:53:53 -0700

    lalale

---
+++ fc++/curlApi.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+using namespace std;
+using std::cout;
+using std::cin;
+
+int main(){
+    
+}
