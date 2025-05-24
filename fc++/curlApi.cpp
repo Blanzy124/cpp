57d2102513f343995ebab99dc343386778d9af0a
Blanzy124 <samuelmedinablandon124@gmail.com>
2025-05-23 22:40:24 -0700

    lalale

--- fc++/curlApi.cpp
+++ fc++/curlApi.cpp
@@ -8,6 +8,20 @@ using namespace std;
 using std::cout;
 using std::cin;
 
+using json = nlohmann::json;
+
+
 int main(){
+   CURL *curl = curl_easy_init();
+  if(curl) {
+    CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_URL, "https://localhost:8443");
+    res = curl_easy_perform(curl);
     
+    cout << curl_easy_perform() << endl;
+    
+    
+    
+    curl_easy_cleanup(curl);
+  }
 }
