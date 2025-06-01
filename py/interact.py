1ddfa2f3ca5e0ee1f3fb886f2d85b5aa72eed3ad
Blanzy124 <samuelmedinablandon124@gmail.com>
2025-05-31 19:16:58 -0700

    lale
    q

--- py/interact.py
+++ py/interact.py
@@ -6,15 +6,36 @@ from OPY import web_s
 import aiohttp;
 import asyncio;
 import ssl
+import serial.tools.list_ports
+
+import pygame
+print(pygame.ver)
+#pygame.init()
+#pygame.joystick.init()
+#
+#if pygame.joystick.get_count() == 0:
+# print("No controllers")
+#else:
+# mando = pygame.joystick.Joystick(0)
+# mando.init()
+# print(f"Detected: {mando.get_name()}")
+# while True:
+#  pygame.event.pump()
+#  
+#  for i in range(mando.get_numbuttons()):
+#   if mando.get_button(i):
+#    print(f"Button: {i} pressed")
+ 
+
+#async def main():
+# option = None
+# while option != 0:
+#  option = int(input("selection"))
+#  match option:
+#   case 0:
+#    print("Closing script")
+#   case 1:
 
 
-
-async def main():
- option = None
- while option != 0:
-  option = int(input("selection"))
-  match option:
-   case 0:
-    print("Closing script")
-   case 1:
+    
     
