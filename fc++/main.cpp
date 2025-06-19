ea2b796f98788d6098fa7fcf66fc649b8eaadf10
Blanzy124 <samuelmedinablandon124@gmail.com>
2025-06-18 21:56:47 -0700

    llale

--- fc++/main.cpp
+++ fc++/main.cpp
@@ -1,41 +1,89 @@
 #include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <string>
 
+using namespace std;
+using std::cout;
+
 namespace beast = boost::beast;
-namespace websocket = beast::websocket;
+namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
 
-int main() {
-    try {
-        std::string host = "echo.websocket.org";
-        std::string port = "80";
-        std::string message = "Hola desde Boost.Beast!";
 
+int main(int argc, char** argv){
+    try
+    {
+        // Check command line arguments.
+        //if(argc != 4 && argc != 5)
+        //{
+        //    std::cerr <<
+        //        "Usage: http-client-sync <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n" <<
+        //        "Example:\n" <<
+        //        "    http-client-sync www.example.com 80 /\n" <<
+        //        "    http-client-sync www.example.com 80 / 1.0\n";
+        //    return EXIT_FAILURE;
+        //}
+        auto const host = "blanzynetwork.org";
+        auto const port = "8443";
+        auto const target = "/";
+        int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
+
+        // The io_context is required for all I/O
         net::io_context ioc;
-        tcp::resolver resolver{ioc};
-        websocket::stream<tcp::socket> ws{ioc};
 
+        // These objects perform our I/O
+        tcp::resolver resolver(ioc);
+        beast::tcp_stream stream(ioc);
+
+        // Look up the domain name
         auto const results = resolver.resolve(host, port);
-        net::connect(ws.next_layer(), results);
 
-        ws.handshake(host, "/");
-        ws.write(net::buffer(message));
+        // Make the connection on the IP address we get from a lookup
+        stream.connect(results);
 
+        // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, target, version};
+        req.set(http::field::host, host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+        // Send the HTTP request to the remote host
+        http::write(stream, req);
+
+        // This buffer is used for reading and must be persisted
         beast::flat_buffer buffer;
-        ws.read(buffer);
 
-        std::cout << "Respuesta del servidor: " << beast::make_printable(buffer.data()) << std::endl;
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
 
-        ws.close(websocket::close_code::normal);
-    } catch (std::exception const& e) {
+        // Receive the HTTP response
+        http::read(stream, buffer, res);
+
+        // Write the message to standard out
+        std::cout << res << std::endl;
+
+        // Gracefully close the socket
+        beast::error_code ec;
+        stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+
+        // not_connected happens sometimes
+        // so don't bother reporting it.
+        //
+        if(ec && ec != beast::errc::not_connected)
+            throw beast::system_error{ec};
+
+        // If we get here then the connection is closed gracefully
+    }
+    catch(std::exception const& e)
+    {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
 }
--- reuse.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def validatedJson(j):
-  try:
-    json.loads(j)
-    return True
-  except (json.JSONDecodeError, TypeError):
-    return False
